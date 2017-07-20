71f7af188195396d230386db4ae79fdfc9e774e8
Mitrij <mbox4email@gmail.com>
2017-07-20 16:41:02 +0300

    Add files via upload

---
+++ js/JS.ga.track.callback.f.EXAMPLE.js
@@ -0,0 +1,86 @@
+// GA callback function
+var callback00 = function(formatted_number, mobile_number)
+{
+	var e1 = document.getElementById("landing_number_link_01");
+	var e2 = document.getElementById("landing_number_link_02");
+	var e3 = document.getElementById("landing_number_link_03");
+	var e4 = document.getElementById("landing_number_link_04");
+	var e5 = document.getElementById("ss_number_link_01");
+	var e6 = document.getElementById("ss_number_link_02");
+	var e7 = document.getElementById("ss_number_link_03");
+	var e10 = document.getElementById("CMAP_number_link_01");
+	var e001 = document.getElementById("faq_number_link_001");
+	var e002 = document.getElementById("faq_number_link_002");
+	var e11 = document.getElementById("faq_number_link_01");
+	var e12 = document.getElementById("faq_number_link_02");
+	var e13 = document.getElementById("faq_number_link_03");
+	var e14 = document.getElementById("faq_number_link_04");
+	var e15 = document.getElementById("faq_number_link_05");
+	var e16 = document.getElementById("faq_number_link_06");
+	var e17 = document.getElementById("faq_number_link_07");
+	if(e1 !== null)
+	{
+		e1.innerHTML = "";
+		e1.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e2 !== null)
+	{
+		e2.innerHTML = "";
+		e2.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e5 !== null)
+	{
+		e5.innerHTML = "";
+		e5.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e10 !== null)
+	{
+		e10.innerHTML = "";
+		e10.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e001 !== null)
+	{
+		e001.innerHTML = "";
+		e001.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e002 !== null)
+	{
+		e002.innerHTML = "";
+		e002.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e11 !== null)
+	{
+		e11.innerHTML = "";
+		e11.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e12 !== null)
+	{
+		e12.innerHTML = "";
+		e12.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e13 !== null)
+	{
+		e13.innerHTML = "";
+		e13.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e14 !== null)
+	{
+		e14.innerHTML = "";
+		e14.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e15 !== null)
+	{
+		e15.innerHTML = "";
+		e15.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e16 !== null)
+	{
+		e16.innerHTML = "";
+		e16.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+	if(e17 !== null)
+	{
+		e17.innerHTML = "";
+		e17.appendChild("CALL US AT " + document.createTextNode(formatted_number));
+	}
+};
